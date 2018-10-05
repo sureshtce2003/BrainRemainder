22aa1e05b6935a79128a4b9ee6fef5f997700fde
sures <sures@LAPTOP-0L8UM4FI>
2018-10-04 23:54:27 -0500

    Initial Setup

---
+++ MVC.BrainRemainder/MVC.BrainRemainder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC.BrainRemainder.MvcApplication" Language="C#" %>
---
+++ MVC.BrainRemainder/MVC.BrainRemainder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.BrainRemainder.Startup))]
+namespace MVC.BrainRemainder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
